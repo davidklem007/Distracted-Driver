ae4d59eb2bc747392973deb3880efa5eaa9d4edf
davidklem007 <davidklem007@gmail.com>
2024-10-16 13:36:48 -0400

    lane change not implemented yet

--- Distracted Driver/Assets/Scripts/PlayerCar.cs
+++ Distracted Driver/Assets/Scripts/PlayerCar.cs
@@ -8,9 +8,13 @@ public class PlayerCar : MonoBehaviour
     bool horizontalPressed = false;
     bool left = false;
     bool right = false;
+    int lane = 3;
     Vector3 oldPos;
     Vector3 newPos;
     [SerializeField] float verticalSpeed;
+    float rightBounds = -2.72f;
+    float leftBounds = -6.12f;
+    float middleBounds = -4.43f;
 
     // Start is called before the first frame update
     void Start()
@@ -34,12 +38,12 @@ public class PlayerCar : MonoBehaviour
     {
         if(transform.position.x < -6.15)
         {
-            transform.position = new Vector3(-6.15f, transform.position.y, transform.position.z);
+            transform.position = new Vector3(leftBounds, transform.position.y, transform.position.z);
             left = false;
         }
         else if(transform.position.x > -2.75)
         {
-            transform.position = new Vector3(-2.75f, transform.position.y, transform.position.z);
+            transform.position = new Vector3(rightBounds, transform.position.y, transform.position.z);
             right = false;
         }
     }
@@ -49,9 +53,9 @@ public class PlayerCar : MonoBehaviour
     //First input has to be released for new input to be detected
     void MoveHorizontal(float input)
     {
-        if (input == -1 && horizontalPressed)
+        if (input == -1 && horizontalPressed && transform.position.x > leftBounds)
         {
-            if (!left && !right && transform.position.x > -6.15)
+            if (!left && !right)
             {
                 oldPos = transform.position;
                 newPos = new Vector3(transform.position.x - 1.70f, transform.position.y, transform.position.z);
@@ -59,7 +63,7 @@ public class PlayerCar : MonoBehaviour
             left = true;
             horizontalPressed = false;
         }
-        else if (input == 1 && horizontalPressed && transform.position.x < -2.75)
+        else if (input == 1 && horizontalPressed && transform.position.x < rightBounds)
         {
             if (!right && !left)
             {
@@ -75,7 +79,7 @@ public class PlayerCar : MonoBehaviour
         }
         if (left)
         {
-            if (!(transform.position.x <= newPos.x) && transform.position.x >= -6.15)
+            if (!(transform.position.x <= newPos.x) && transform.position.x >= leftBounds)
             {
                 transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed);
             }
@@ -86,7 +90,7 @@ public class PlayerCar : MonoBehaviour
         }
         else if (right)
         {
-            if (!(transform.position.x >= newPos.x) && transform.position.x <= -2.75)
+            if (!(transform.position.x >= newPos.x) && transform.position.x <= rightBounds)
             {
                 transform.Translate(Vector3.right * Time.deltaTime * horizontalSpeed);
             }
@@ -112,5 +116,96 @@ public class PlayerCar : MonoBehaviour
             transform.position = new Vector3(transform.position.x, 4.3f, transform.position.z);
         }
         
+    }
+
+    void LaneChange(float input)
+    {
+        int intput = Mathf.RoundToInt(input);
+        lane = Clamp3(intput + lane);
+    }
+
+    int Clamp3(int num)
+    {
+        if (num > 3)
+        {
+            return 3;
+        }
+        if(num < 1)
+        {
+            return 1;
+        }
+        return num;
+
+    }
+
+    void LaneClamp()
+    {
+        if(lane == 1)
+        {
+            if (left)
+            {
+                if(transform.position.x < leftBounds)
+                {
+                    left = false;
+                    transform.position = new Vector3(leftBounds, transform.position.y, transform.position.z);
+                }
+            }
+            else
+            {
+                if (transform.position.x != leftBounds)
+                {
+                    left = false;
+                    transform.position = new Vector3(leftBounds, transform.position.y, transform.position.z);
+                }
+            }
+        }
+        else if(lane == 2)
+        {
+            if (left)
+            {
+                if (transform.position.x < middleBounds)
+                {
+                    left = false;
+                    transform.position = new Vector3(middleBounds, transform.position.y, transform.position.z);
+                }
+            }
+            else if(right)
+            {
+                if (transform.position.x > middleBounds)
+                {
+                    right = false;
+                    transform.position = new Vector3(middleBounds, transform.position.y, transform.position.z);
+                }
+            }
+            else
+            {
+                if (transform.position.x != leftBounds)
+                {
+                    left = false;
+                    right = false;
+                    transform.position = new Vector3(leftBounds, transform.position.y, transform.position.z);
+                }
+            }
+        }
+        else
+        {
+            if (right)
+            {
+                if (transform.position.x > rightBounds)
+                {
+                    right = false;
+                    transform.position = new Vector3(rightBounds, transform.position.y, transform.position.z);
+                }
+            }
+            else
+            {
+                if (transform.position.x != rightBounds)
+                {
+                    right = false;
+                    transform.position = new Vector3(rightBounds, transform.position.y, transform.position.z);
+                }
+            }
+        }
+            
     }
 }
