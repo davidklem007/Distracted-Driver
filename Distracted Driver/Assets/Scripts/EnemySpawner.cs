1602af2e92fe734b385d04160c5fe226cecc1b6f
davidklem007 <davidklem007@gmail.com>
2024-10-17 22:02:12 -0400

    Different enemy cars, restart button

--- Distracted Driver/Assets/Scripts/EnemySpawner.cs
+++ Distracted Driver/Assets/Scripts/EnemySpawner.cs
@@ -8,7 +8,9 @@ public class EnemySpawner : MonoBehaviour
     //
     //!!to do: create prefab variants for all different enemies
 
-    [SerializeField] GameObject enemyPrefab;
+    GameObject enemyPrefab;
+    GameObject enemyPrefab2;
+    GameObject[] enemyPrefabs;
     float rightBounds = -2.72f;
     float leftBounds = -6.14f;
     float middleBounds = -4.43f;
@@ -17,6 +19,8 @@ public class EnemySpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        enemyPrefabs = Resources.LoadAll<GameObject>("Enemy Cars");
+
         InvokeRepeating("SpawnEnemy", 2, spawnRate);
         EventManager.GameOver.AddListener(Stop);
     }
@@ -27,10 +31,15 @@ public class EnemySpawner : MonoBehaviour
         Random.InitState((int) System.DateTime.Now.Ticks);
     }
 
+    //1 or 2 random enemy prefabs spawn in 1 or 2 different lanes
     void SpawnEnemy()
     {
+        enemyPrefab = enemyPrefabs[Random.Range(0, enemyPrefabs.Length)];
+        enemyPrefab2 = enemyPrefabs[Random.Range(0, enemyPrefabs.Length)];
+
         int lane = Random.Range(1, 7);
 
+        //instantiate enemy prefabs in random lanes
         switch (lane)
         {
             case 1:
@@ -44,15 +53,15 @@ public class EnemySpawner : MonoBehaviour
                 break;
             case 4:
                 Instantiate(enemyPrefab, new Vector3(rightBounds, 9.5f, 0), Quaternion.identity);
-                Instantiate(enemyPrefab, new Vector3(leftBounds, 9.5f, 0), Quaternion.identity);
+                Instantiate(enemyPrefab2, new Vector3(leftBounds, 9.5f, 0), Quaternion.identity);
                 break;
             case 5:
                 Instantiate(enemyPrefab, new Vector3(rightBounds, 9.5f, 0), Quaternion.identity);
-                Instantiate(enemyPrefab, new Vector3(middleBounds, 9.5f, 0), Quaternion.identity);
+                Instantiate(enemyPrefab2, new Vector3(middleBounds, 9.5f, 0), Quaternion.identity);
                 break;
             case 6:
                 Instantiate(enemyPrefab, new Vector3(leftBounds, 9.5f, 0), Quaternion.identity);
-                Instantiate(enemyPrefab, new Vector3(middleBounds, 9.5f, 0), Quaternion.identity);
+                Instantiate(enemyPrefab2, new Vector3(middleBounds, 9.5f, 0), Quaternion.identity);
                 break;
 
         }
--- Distracted Driver/Assets/Scripts/GameManager.cs
+++ Distracted Driver/Assets/Scripts/GameManager.cs
@@ -1,20 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] GameObject roadLines;
 
+    [SerializeField] GameObject restartButton;
     // Start is called before the first frame update
     void Start()
     {
-        
+        EventManager.GameOver.AddListener(GameOver);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void GameOver()
+    {
+        restartButton.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
